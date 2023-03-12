19641e4f3f5db37aba6a2233bd29438e3a0fd635
Jose Carreres <jose.carreres.81@gmail.com>
2023-03-12 07:02:24 +0100

    Crear usuario

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
@@ -24,6 +25,11 @@ class RegisterController extends Controller
             //'name' => ['required', 'max:30'],
         ]);
 
-        dd('Creando usuario');
+        User::create([
+            'name'     => $request->name,
+            'username' => $request->username,
+            'email'    => $request->email,
+            'password' => $request->password
+        ]);
     }
 }
